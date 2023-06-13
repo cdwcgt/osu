d7782592159f48bc2fb6c4d7f7c61294a4467574
cdwcgt <cdwcgt@cdwcgt.top>
2023-06-13 23:03:44 +0900

    simply GetAngle and add xmldoc

--- osu.Game.Rulesets.Osu/Replays/Mover/FlowerMover.cs
+++ osu.Game.Rulesets.Osu/Replays/Mover/FlowerMover.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Objects;
+using osu.Game.Rulesets.Objects.Types;
 using osu.Game.Rulesets.Osu.Objects;
 using osuTK;
 
@@ -20,64 +22,74 @@ namespace osu.Game.Rulesets.Osu.Replays.Mover
         private float invert = 1;
         private float lastAngle;
         private Vector2 lastPoint;
-        private BezierCurveCubic curve;
+        private SliderPath? path;
 
         public FlowerMover(IBeatmap beatmap, IReadOnlyList<Mod> mods, float jumpMultiplier, float angleOffset)
             : base(beatmap, mods)
         {
-            mult = jumpMultiplier;
-            nextMult = jumpMultiplier;
+            mult = nextMult = jumpMultiplier;
             offsetMult = angleOffset;
         }
 
         protected override void OnObjChange()
         {
             Vector2 p1, p2;
-            float dist = Vector2.Distance(CurrentPostion, TargetPostion);
-            float scaled = mult * dist;
-            float next = nextMult * dist;
+            float distance = Vector2.Distance(CurrentPosition, TargetPosition);
+            float scaledDistance = mult * distance;
+            float nextScaledDistance = nextMult * distance;
 
+            // check if object is a Slider.
             Slider? start = CurrentObject as Slider;
             Slider? end = TargetObject as Slider;
 
             float newAngle = offset * invert;
 
+            // start object and end object are all slider.
             if (start != null && end != null)
             {
                 invert *= -1;
-                p1 = MoverUtilExtensions.V2FromRad(start.GetEndAngle(), scaled) + CurrentPostion;
-                p2 = MoverUtilExtensions.V2FromRad(end.GetStartAngle(), next) + TargetPostion;
+                p1 = MoverUtilExtensions.V2FromRad(start.GetAngle(true), scaledDistance) + CurrentPosition;
+                p2 = MoverUtilExtensions.V2FromRad(end.GetAngle(), nextScaledDistance) + TargetPosition;
             }
+            // start object is slider.
             else if (start != null)
             {
                 invert *= -1;
-                lastAngle = CurrentPostion.AngleRV(TargetPostion) - newAngle;
+                lastAngle = CurrentPosition.AngleRV(TargetPosition) - newAngle;
 
-                p1 = MoverUtilExtensions.V2FromRad(start.GetEndAngle(), scaled) + CurrentPostion;
-                p2 = MoverUtilExtensions.V2FromRad(lastAngle, next) + TargetPostion;
+                p1 = MoverUtilExtensions.V2FromRad(start.GetAngle(true), scaledDistance) + CurrentPosition;
+                p2 = MoverUtilExtensions.V2FromRad(lastAngle, nextScaledDistance) + TargetPosition;
             }
+            // end object is slider.
             else if (end != null)
             {
                 lastAngle += MathF.PI;
-                p1 = MoverUtilExtensions.V2FromRad(lastAngle, scaled) + CurrentPostion;
-                p2 = MoverUtilExtensions.V2FromRad(end.GetStartAngle(), next) + TargetPostion;
+                p1 = MoverUtilExtensions.V2FromRad(lastAngle, scaledDistance) + CurrentPosition;
+                p2 = MoverUtilExtensions.V2FromRad(end.GetAngle(), nextScaledDistance) + TargetPosition;
             }
+            // all are not slider.
             else
             {
-                if (MoverUtilExtensions.AngleBetween(CurrentPostion, lastPoint, TargetPostion) >= offset)
+                if (MoverUtilExtensions.AngleBetween(CurrentPosition, lastPoint, TargetPosition) >= offset)
                     invert *= -1;
 
-                newAngle = CurrentPostion.AngleRV(TargetPostion) - newAngle;
+                newAngle = CurrentPosition.AngleRV(TargetPosition) - newAngle;
 
-                p1 = MoverUtilExtensions.V2FromRad(lastAngle + MathF.PI, scaled) + CurrentPostion;
-                p2 = MoverUtilExtensions.V2FromRad(newAngle, next) + TargetPostion;
-                if (scaled / mult > 2) lastAngle = newAngle;
+                p1 = MoverUtilExtensions.V2FromRad(lastAngle + MathF.PI, scaledDistance) + CurrentPosition;
+                p2 = MoverUtilExtensions.V2FromRad(newAngle, nextScaledDistance) + TargetPosition;
+                if (scaledDistance / mult > 2) lastAngle = newAngle;
             }
 
-            lastPoint = CurrentPostion;
-            curve = new BezierCurveCubic(CurrentPostion, TargetPostion, p1, p2);
+            lastPoint = CurrentPosition;
+            path = new SliderPath(new[]
+            {
+                new PathControlPoint(CurrentPosition, PathType.Bezier),
+                new PathControlPoint(p1),
+                new PathControlPoint(p2),
+                new PathControlPoint(TargetPosition)
+            });
         }
 
-        protected override Vector2 Update(double time) => curve.CalculatePoint((float)((time - CurrentObjectTime) / Duration));
+        protected override Vector2 GetPosition(double time) => path?.PositionAt((float)((time - CurrentObjectTime) / Duration)) ?? Vector2.One;
     }
 }
