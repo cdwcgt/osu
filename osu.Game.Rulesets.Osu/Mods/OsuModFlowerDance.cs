6bf75cab85325035451d1ad39335c8b7c0a19084
cdwcgt <cdwcgt@cdwcgt.top>
2023-06-14 01:27:27 +0900

    do not inherit from ModAutoPlay
    will cause strange issues if inherit

--- osu.Game.Rulesets.Osu/Mods/OsuModFlowerDance.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModFlowerDance.cs
@@ -4,44 +4,58 @@
 using osu.Game.Beatmaps;
 using System.Collections.Generic;
 using System;
-using System.Linq;
 using osu.Framework.Bindables;
+using osu.Framework.Localisation;
 using osu.Game.Configuration;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Osu.Replays.Mover;
 
-namespace osu.Game.Rulesets.Osu.Mods;
-
-public class OsuModFlowerDance : ModAutoplay
+namespace osu.Game.Rulesets.Osu.Mods
 {
-    public override string Name => "Flower Dance";
-
-    public override string Acronym => "FD";
-
-    public override bool RequiresConfiguration => true;
-
-    [SettingSource("Jump multiplier")]
-    public BindableFloat JumpMultiplier { get; set; } = new BindableFloat()
+    public class OsuModFlowerDance : Mod, ICreateReplayData, IApplicableFailOverride
     {
-        Value = 0.6f,
-        Default = 0.6f,
-        MinValue = 0f,
-        MaxValue = 2f,
-        Precision = 0.01f,
-    };
+        public override string Name => "Flower Dance";
 
-    [SettingSource("Angle offset")]
-    public BindableFloat AngleOffset { get; set; } = new BindableFloat
-    {
-        Value = 0.45f,
-        Default = 0.45f,
-        MinValue = 0f,
-        MaxValue = 2f,
-        Precision = 0.01f,
-    };
+        public override string Acronym => "FD";
 
-    public override Type[] IncompatibleMods => base.IncompatibleMods.Concat(new[] { typeof(OsuModMagnetised), typeof(OsuModRepel), typeof(OsuModAutopilot), typeof(OsuModSpunOut), typeof(OsuModAlternate), typeof(OsuModSingleTap), typeof(OsuModAutoplay) }).ToArray();
+        public override ModType Type => ModType.Automation;
 
-    public override ModReplayData CreateReplayData(IBeatmap beatmap, IReadOnlyList<Mod> mods)
-        => new ModReplayData(new FlowerMover(beatmap, mods, JumpMultiplier.Value, AngleOffset.Value).Generate(), new ModCreatedUser { Username = "lazer!dance" });
+        public override LocalisableString Description => "Enjoy the beautiful curve.";
+        public override double ScoreMultiplier => 1;
+
+        public bool PerformFail() => false;
+
+        public bool RestartOnFail => false;
+
+        public override bool UserPlayable => false;
+        public override bool ValidForMultiplayer => false;
+        public override bool ValidForMultiplayerAsFreeMod => false;
+
+        public override bool RequiresConfiguration => true;
+
+        [SettingSource("Jump multiplier")]
+        public BindableFloat JumpMultiplier { get; set; } = new BindableFloat
+        {
+            Value = 0.6f,
+            Default = 0.6f,
+            MinValue = 0f,
+            MaxValue = 2f,
+            Precision = 0.01f,
+        };
+
+        [SettingSource("Angle offset")]
+        public BindableFloat AngleOffset { get; set; } = new BindableFloat
+        {
+            Value = 0.45f,
+            Default = 0.45f,
+            MinValue = 0f,
+            MaxValue = 2f,
+            Precision = 0.01f,
+        };
+
+        public override Type[] IncompatibleMods => new[] { typeof(ModCinema), typeof(ModRelax), typeof(ModFailCondition), typeof(ModNoFail), typeof(ModAdaptiveSpeed), typeof(OsuModMagnetised), typeof(OsuModRepel), typeof(OsuModAutopilot), typeof(OsuModSpunOut), typeof(OsuModAlternate), typeof(OsuModSingleTap), typeof(OsuModAutoplay), typeof(OsuModNoScope)};
+
+        public ModReplayData CreateReplayData(IBeatmap beatmap, IReadOnlyList<Mod> mods)
+            => new ModReplayData(new FlowerMover(beatmap, mods, JumpMultiplier.Value, AngleOffset.Value).Generate(), new ModCreatedUser { Username = "lazer!dance" });
+    }
 }
