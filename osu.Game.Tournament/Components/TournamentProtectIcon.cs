b64affa2040f1df837306d1c4b13b725ee92cb8a
cdwcgt <cdwcgt@cdwcgt.top>
2024-03-11 22:51:55 +0900

    add TournamentProtectIcon

---
+++ osu.Game.Tournament/Components/TournamentProtectIcon.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using osu.Game.Tournament.Models;
+using osuTK;
+
+namespace osu.Game.Tournament.Components
+{
+    public partial class TournamentProtectIcon : CompositeDrawable
+    {
+        private readonly TeamColour color;
+
+        public TournamentProtectIcon(TeamColour colour)
+        {
+            this.color = colour;
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(TextureStore textures)
+        {
+            var customTexture = textures.Get($"Protect/team-{(color == TeamColour.Red ? "red" : "blue")}");
+
+            if (customTexture != null)
+            {
+                AddInternal(new Sprite
+                {
+                    FillMode = FillMode.Fit,
+                    RelativeSizeAxes = Axes.Both,
+                    Anchor = Anchor.CentreRight,
+                    Origin = Anchor.CentreRight,
+                    Texture = customTexture
+                });
+                return;
+            }
+
+            AddInternal(new SpriteIcon
+            {
+                Origin = Anchor.Centre,
+                Anchor = Anchor.Centre,
+                Size = new Vector2(45),
+                Icon = FontAwesome.Solid.Question
+            });
+        }
+    }
+}
