2fd6dc96d7c240b76d27865fa36766aaf9aa87ec
cdwcgt <cdwcgt@cdwcgt.top>
2024-10-05 01:36:45 +0800

    1

--- osu.Game.Tournament/Screens/Gameplay/Components/RoundInformationPreview.cs
+++ osu.Game.Tournament/Screens/Gameplay/Components/RoundInformationPreview.cs
@@ -1,10 +1,18 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Schema;
+using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Game.Graphics;
+using osu.Game.Overlays.Music;
+using osu.Game.Tournament.Models;
 using osuTK;
 using osuTK.Graphics;
 
@@ -12,6 +20,7 @@ namespace osu.Game.Tournament.Screens.Gameplay.Components
 {
     public partial class RoundInformationPreview : CompositeDrawable
     {
+        private readonly FillFlowContainer mapContentContainer;
         private static readonly Color4 boarder_color = new Color4(56, 56, 56, 255);
 
         public RoundInformationPreview()
@@ -79,23 +88,27 @@ namespace osu.Game.Tournament.Screens.Gameplay.Components
                     RelativeSizeAxes = Axes.Y,
                     Children = new Drawable[]
                     {
-                        new FillFlowContainer
+                        mapContentContainer = new FillFlowContainer
                         {
-                            Anchor = Anchor.Centre,
-                            Origin = Anchor.Centre,
+                            Anchor = Anchor.TopCentre,
+                            Origin = Anchor.TopCentre,
                             AutoSizeAxes = Axes.Both,
-                            Direction = FillDirection.Vertical,
-                            Children = new Drawable[]
-                            {
-                                createHeaderSection("禁图"),
-                            }
+                            Direction = FillDirection.Horizontal
                         }
                     }
                 }
             };
+
+            var mapDetail = new MapDetailContent();
+            mapContentContainer.Add(mapDetail);
+            List<BeatmapChoice> choices = new List<BeatmapChoice>();
+
+            for (int i = 0; i < 4; i++) choices.Add(new BeatmapChoice());
+
+            mapDetail.UpdateBeatmap(choices);
         }
 
-        private Drawable createHeaderSection(string text)
+        private static Drawable createHeaderSection(string text)
         {
             return new FillFlowContainer
             {
@@ -138,28 +151,126 @@ namespace osu.Game.Tournament.Screens.Gameplay.Components
             };
         }
 
-        private Drawable createMapBox(string mapName, Color4 color)
+        private static Drawable createMapBoxContent(string mapName, Color4 backgroundColor, Color4 textColor)
         {
             return new Container
             {
-                AutoSizeAxes = Axes.Both,
+                RelativeSizeAxes = Axes.Both,
                 Children = new Drawable[]
                 {
                     new Box
                     {
-                        Size = new Vector2(60, 30),
-                        Colour = color,
+                        RelativeSizeAxes = Axes.Both,
+                        Colour = backgroundColor,
                     },
                     new TournamentSpriteText
                     {
                         Text = mapName,
                         Anchor = Anchor.Centre,
                         Origin = Anchor.Centre,
-                        Colour = Color4.White,
-                        Font = OsuFont.Torus.With(size: 18)
+                        Colour = textColor,
+                        Font = OsuFont.Torus.With(size: 25),
+                        Shadow = true,
                     }
                 }
             };
+        }
+
+        private partial class MapDetailContent : CompositeDrawable
+        {
+            private FillFlowContainer mapContent = null!;
+
+            [Resolved]
+            private LadderInfo ladderInfo { get; set; } = null!;
+
+            [BackgroundDependencyLoader]
+            private void load()
+            {
+                InternalChild = new FillFlowContainer
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    AutoSizeAxes = Axes.Both,
+                    Direction = FillDirection.Vertical,
+                    Children = new Drawable[]
+                    {
+                        createHeaderSection("禁图"),
+                        mapContent = new FillFlowContainer
+                        {
+                            Direction = FillDirection.Horizontal,
+                            AutoSizeAxes = Axes.Both,
+                        }
+                    }
+                };
+            }
+
+            public void UpdateBeatmap(IEnumerable<BeatmapChoice> maps)
+            {
+                if (ladderInfo.CurrentMatch.Value?.Round.Value == null)
+                    return;
+
+                mapContent.Clear();
+
+                mapContent.ChildrenEnumerable = maps.Select(map =>
+                {
+                    var mapBox = new MapBox();
+
+                    //var roundBeatmap = ladderInfo.CurrentMatch.Value.Round.Value?.Beatmaps.FirstOrDefault(roundMap => roundMap.ID == map.BeatmapID);
+                    var roundBeatmap = ladderInfo.CurrentMatch.Value.Round.Value?.Beatmaps.FirstOrDefault();
+                    if (roundBeatmap == null)
+                        return mapBox;
+
+                    mapBox.Colour = map.Team == TeamColour.Red
+                        ? new Color4(212, 48, 48, 255)
+                        : new Color4(42, 130, 228, 255);
+
+                    Color4 backgroundColor = map.Type == ChoiceType.Ban ? Color4.Gray : roundBeatmap.BackgroundColor;
+                    Color4 textColor = map.Type == ChoiceType.Ban ? new Color4(229, 229, 229, 255) : roundBeatmap.TextColor;
+                    var mapBoxContent = createMapBoxContent(roundBeatmap.Mods, textColor, textColor);
+                    return mapBox;
+                });
+            }
+        }
+
+        private partial class MapBox : CompositeDrawable
+        {
+            public MapBox()
+            {
+                Height = 50;
+                Width = 42;
+
+                InternalChildren = new Drawable[]
+                {
+                    TopMapContainer = new Container
+                    {
+                        Anchor = Anchor.TopCentre,
+                        Origin = Anchor.TopCentre,
+                        Height = 18,
+                        RelativeSizeAxes = Axes.X,
+                    },
+                    CenterLine = new Box
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Height = 3.6f,
+                        RelativeSizeAxes = Axes.X,
+                        Colour = Color4.Gray,
+                    },
+                    BottomMapContainer = new Container
+                    {
+                        Anchor = Anchor.BottomCentre,
+                        Origin = Anchor.BottomCentre,
+                        Height = 18,
+                        RelativeSizeAxes = Axes.X,
+                    },
+                };
+            }
+
+            public Container TopMapContainer { get; set; }
+
+            public Container BottomMapContainer { get; set; }
+
+            public Box CenterLine { get; set; }
         }
     }
 }
