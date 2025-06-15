50938263c694d42a115610faca4c6a0301946cfb
cdwcgt <cdwcgt@cdwcgt.top>
2025-06-15 22:29:57 +0800

    错了哥

--- osu.Game.Tournament/Screens/Gameplay/GameplayScreen.cs
+++ osu.Game.Tournament/Screens/Gameplay/GameplayScreen.cs
@@ -109,14 +109,12 @@ namespace osu.Game.Tournament.Screens.Gameplay
                             Text = "Toggle chat",
                             Action = () => { State.Value = State.Value == TourneyState.Idle ? TourneyState.Playing : TourneyState.Idle; }
                         },
-                        OperatingSystem.IsWindows()
-                            ? Empty()
-                            : new SettingsSlider<int>
-                            {
-                                LabelText = "Chroma width",
-                                Current = LadderInfo.ChromaKeyWidth,
-                                KeyboardStep = 1,
-                            },
+                        new SettingsSlider<int>
+                        {
+                            LabelText = $"{(OperatingSystem.IsWindows() ? "Player Area" : "Chroma")} width",
+                            Current = LadderInfo.ChromaKeyWidth,
+                            KeyboardStep = 1,
+                        },
                         OperatingSystem.IsWindows()
                             ? new SettingsSlider<int>
                             {
@@ -135,8 +133,7 @@ namespace osu.Game.Tournament.Screens.Gameplay
                 }
             });
 
-            if (!OperatingSystem.IsWindows())
-                LadderInfo.ChromaKeyWidth.BindValueChanged(width => chroma.Width = width.NewValue, true);
+            LadderInfo.ChromaKeyWidth.BindValueChanged(width => chroma.Width = width.NewValue, true);
 
             warmup.BindValueChanged(w =>
             {
