c79d263201641a0c5c557bf65befb6167849086d
cdwcgt <cdwcgt@cdwcgt.top>
2025-08-03 23:31:20 +0800

    删除不必要的using

--- osu.Game.Tournament/Components/CapturedWindowSprite.cs
+++ osu.Game.Tournament/Components/CapturedWindowSprite.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.Buffers;
 using System.Runtime.InteropServices;
 using osu.Framework.Graphics;
 using System.Drawing;
