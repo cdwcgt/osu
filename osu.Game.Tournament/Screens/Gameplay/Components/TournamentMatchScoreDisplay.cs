58501ac278ef390649e0d2c427222dac248afbed
cdwcgt <cdwcgt@cdwcgt.top>
2025-08-13 20:27:28 +0800

    笑了 不小心加进去了

--- osu.Game.Tournament/Screens/Gameplay/Components/TournamentMatchScoreDisplay.cs
+++ osu.Game.Tournament/Screens/Gameplay/Components/TournamentMatchScoreDisplay.cs
@@ -4,44 +4,16 @@
 using osu.Framework.Allocation;
 using osu.Game.Screens.Play.HUD;
 using osu.Game.Tournament.IPC;
-using osu.Game.Screens.Play.HUD;
-using osu.Game.Tournament.IPC;
-using osu.Game.Tournament.Models;
 
 namespace osu.Game.Tournament.Screens.Gameplay.Components
 {
     public partial class TournamentMatchScoreDisplay : MatchScoreDisplay
     {
-        private bool invertTextColor;
-        private readonly Colour4 black = Colour4.FromHex("1f1f1f");
-
-        [Resolved]
-        private LadderInfo ladderInfo { get; set; } = null!;
-
-        [Resolved]
-        private MatchIPCInfo ipc { get; set; } = null!;
-
-        public bool InvertTextColor
-        {
-            get => invertTextColor;
-            set
-            {
-                invertTextColor = value;
-                updateColor();
-            }
-        }
-
         [BackgroundDependencyLoader]
         private void load(MatchIPCInfo ipc)
         {
             Team1Score.BindTo(ipc.Score1);
             Team2Score.BindTo(ipc.Score2);
-        }
-
-        private void updateColor()
-        {
-            var color = invertTextColor ? black : Colour4.White;
-            Score1Text.Colour = Score2Text.Colour = ScoreDiffText.Colour = color;
         }
     }
 }
